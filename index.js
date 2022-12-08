a7760ff0d9f773c2d6886ea62c7dbcc40c19b9b0
zakariyaahmed123 <118251771+zakariyaahmed123@users.noreply.github.com>
2022-12-08 10:29:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+// function countDown(number) {
+//     for (let i = number; i >= 0; i--){
+//         console.log(i);
+//     }
+// }
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
